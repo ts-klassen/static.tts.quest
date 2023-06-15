04ba451ccb300c077856fc67f0eb29c30a9e6700
ts-klassen <git@su-shiki.com>
2023-06-16 00:32:46 +0900

    rm non .txt files

--- voicevox_speakers_by_id/_middleware.js
+++
@@ -1,5 +0,0 @@
-export const onRequest: PagesFunction = async ({ next }) => {
-  const response = await next();
-  response.headers.set('content-type', 'text/html; charset=UTF-8');
-  return response;
-};
